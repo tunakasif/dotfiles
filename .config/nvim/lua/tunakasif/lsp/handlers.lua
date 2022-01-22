6d26ea552b452b93f7e166ed39498124f38542dc
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2022-01-23 01:11:13 +0300

    nvim: Update LSP keybindings
    Mapping was set with hardcoded <space>. They are changed to <leader>

--- .config/nvim/lua/tunakasif/lsp/handlers.lua
+++ .config/nvim/lua/tunakasif/lsp/handlers.lua
@@ -85,25 +85,27 @@ local function lsp_keymaps(bufnr)
 	buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
 	buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
 	buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	buf_set_keymap(bufnr, "n", "<space>k", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	buf_set_keymap(bufnr, "n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-	buf_set_keymap(bufnr, "n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+	buf_set_keymap(bufnr, "n", "<leader>k", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	buf_set_keymap(bufnr, "n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+	buf_set_keymap(bufnr, "n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
 	buf_set_keymap(
 		bufnr,
 		"n",
-		"<space>wl",
+		"<leader>wl",
 		"<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
 		opts
 	)
-	buf_set_keymap(bufnr, "n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-	buf_set_keymap(bufnr, "n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-	buf_set_keymap(bufnr, "n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+	buf_set_keymap(bufnr, "n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+	buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 	buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	buf_set_keymap(bufnr, "n", "<space>d", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+	buf_set_keymap(bufnr, "n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 	buf_set_keymap(bufnr, "n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
 	buf_set_keymap(bufnr, "n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-	buf_set_keymap(bufnr, "n", "<space>sl", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-	buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+	buf_set_keymap(bufnr, "n", "<leader>sl", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+	buf_set_keymap(bufnr, "n", "<leader>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+	buf_set_keymap(bufnr, "n", "<leader>fs", ":lua TOGGLE_FORMAT_ON_SAVE()<CR>", opts)
+
 	vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()']])
 end
 
