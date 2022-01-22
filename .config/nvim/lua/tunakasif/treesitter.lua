220e9c5c826fcbea680490dbb53aa1f4c3f53960
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2022-01-23 01:01:25 +0300

    nvim: Simplify treesitter autotag

--- .config/nvim/lua/tunakasif/treesitter.lua
+++ .config/nvim/lua/tunakasif/treesitter.lua
@@ -12,15 +12,6 @@ configs.setup {
     },
     autotag = {
         enable = true,
-        filetypes = {
-            'html', 'javascript', 'javascriptreact',
-            'typescriptreact', 'svelte', 'vue'
-        },
-        skip_tags = {
-            'area', 'base', 'br', 'col', 'command', 'embed',
-            'hr', 'img', 'slot', 'input', 'keygen', 'link',
-            'meta', 'param', 'source', 'track', 'wbr','menuitem'
-        },
     },
     highlight = {
         enable = true,
