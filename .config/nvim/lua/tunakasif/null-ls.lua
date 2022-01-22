75375569af59dc1a05b5bb1f55586ac87eca385d
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2022-01-23 01:14:44 +0300

    nvim: FIx formatting of latex
    Formatting with null-ls for latex files have some trouble with
    overwriting the current buffer. Therefore, formatting of the latex files
    handled with LSP server texlab.

--- .config/nvim/lua/tunakasif/null-ls.lua
+++ .config/nvim/lua/tunakasif/null-ls.lua
@@ -1,20 +1,20 @@
 local status_ok, null_ls = pcall(require, "null-ls")
 if not status_ok then
-    return
+	return
 end
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.latexindent.with(
-            {
-                extra_args = {
-                    ""
-                }
-            }
-        ),
-        null_ls.builtins.diagnostics.flake8,
-        null_ls.builtins.completion.spell,
-    },
+	sources = {
+		-- Formatter
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.stylua,
+
+		-- Diagnostics
+		null_ls.builtins.diagnostics.flake8,
+		null_ls.builtins.diagnostics.chktex,
+
+		-- Other
+		null_ls.builtins.completion.spell,
+	},
 })
