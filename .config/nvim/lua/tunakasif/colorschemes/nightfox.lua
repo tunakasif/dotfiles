911f44076ed64b802eb52d2cec296a3449a6adee
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2022-03-17 23:34:04 +0300

    Remove nightfox config due to breakage

--- .config/nvim/lua/tunakasif/colorschemes/nightfox.lua
+++ .config/nvim/lua/tunakasif/colorschemes/nightfox.lua
@@ -3,15 +3,9 @@ if not status_ok then
 	return
 end
 
-nightfox.setup({
-	fox = "nordfox", -- nightfox, nordfox, dayfox, dawnfox, duskfox
-	transparent = false,
-	styles = {
-		comments = "italic", -- change style of comments to be italic
-		keywords = "italic", -- change style of keywords to be bold
-		functions = "NONE", -- styles can be a comma separated list_workspace_folders
-	},
-})
+-- Default options
+-- Default options
+require("nightfox").setup({})
 
--- Load the configuration set above and apply the colorscheme
-nightfox.load()
+-- setup must be called before loading
+vim.cmd("colorscheme nordfox")
