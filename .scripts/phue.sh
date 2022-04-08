d7a7d26acef018a0388cf60d781cf9411c8e668f
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2022-04-08 19:40:48 +0300

    phue: Add switching off func to scene-setter

--- .scripts/phue.sh
+++ .scripts/phue.sh
@@ -1,6 +1,12 @@
 phue() {
+    scene="$1"
+    scene_lower="$(echo "$scene" | tr '[:upper:]' '[:lower:]')"
     curr_path=$(pwd);
     cd $HOME/Documents/GitHub/phue_control;
-    poetry run python3 -m phue_control set-scene-by-name "$1";
+    if [[ "$scene_lower" = "off"  ]]; then
+       poetry run python3 -m phue_control switch all off;
+    else
+        poetry run python3 -m phue_control set-scene-by-name "$scene";
+    fi
     cd "$curr_path";
 }
