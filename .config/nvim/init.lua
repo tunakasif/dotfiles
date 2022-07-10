f7e05336c73bcd9b8d5bccf3aca541a5855ea2cc
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2022-07-10 18:04:27 +0300

    Change formatting of  to null-ls

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,38 +1,39 @@
 local namespace = "tunakasif"
 local colorschemes = {
-    "darkplus",
-    "gruvbox-baby",
-    "gruvbox-material",
-    "nightfox",
-    "tokyonight",
-    "vscode",
+	"darkplus",
+	"gruvbox-baby",
+	"gruvbox-material",
+	"nightfox",
+	"tokyonight",
+	"vscode",
 }
 
 local lua_scripts = {
-    "colorschemes." .. colorschemes[4],
-    "options",
-    "plugins",
-    "autopairs",
-    "bufferline",
-    "cmp",
-    "copilot",
-    "gitsigns",
-    "impatient",
-    "lsp",
-    "lualine",
-    -- "luasnip",
-    -- "null-ls",
-    "nvim-tree",
-    "telescope",
-    "toggleterm",
-    "transparent.transparent",
-    "treesitter",
-    "vimtex",
-    "zenmode",
-    "keymaps",
+	"colorschemes." .. colorschemes[4],
+	"options",
+	"plugins",
+	"alpha",
+	"autopairs",
+	"bufferline",
+	"cmp",
+	"copilot",
+	"gitsigns",
+	"impatient",
+	"lsp",
+	"lualine",
+	"luasnip",
+	"null-ls",
+	"nvim-tree",
+	"telescope",
+	"toggleterm",
+	"transparent.transparent",
+	"treesitter",
+	"vimtex",
+	"zenmode",
+	"keymaps",
 }
 
 for i = 1, #lua_scripts do
-    local module = namespace .. "." .. lua_scripts[i]
-    require(module)
+	local module = namespace .. "." .. lua_scripts[i]
+	require(module)
 end
