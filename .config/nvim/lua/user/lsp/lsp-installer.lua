31532845e7f042757cc10518656139cdfd880d53
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2022-07-14 02:08:00 +0300

    fix(lsp-installer): update lsp-installer config
    LSP-installer config was containing deprecated functions, so they are
    removed according to [chris@machine]'s setup.
    
    [chris@machine]: https://www.chrisatmachine.com

--- .config/nvim/lua/user/lsp/lsp-installer.lua
+++ .config/nvim/lua/user/lsp/lsp-installer.lua
@@ -1,31 +1,45 @@
-local namespace = "user.lsp."
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not status_ok then
 	return
 end
 
-local function has_value(tab, val)
-	for _, value in ipairs(tab) do
-		if value == val then
-			return true
-		end
-	end
-	return false
+local servers = {
+	"sumneko_lua",
+	"cssls",
+	"html",
+	"tsserver",
+	"pyright",
+	"bashls",
+	"jsonls",
+	"yamlls",
+	"rust_analyzer",
+	"taplo",
+}
+
+lsp_installer.setup()
+
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+	return
 end
 
-lsp_installer.on_server_ready(function(server)
-	local handlers = namespace .. "handlers"
-	local opts = {
-		on_attach = require(handlers).on_attach,
-		capabilities = require(handlers).capabilities,
+local opts = {}
+
+for _, server in pairs(servers) do
+	opts = {
+		on_attach = require("user.lsp.handlers").on_attach,
+		capabilities = require("user.lsp.handlers").capabilities,
 	}
 
-	local server_names = { "clangd", "pyright", "sumneko_lua" }
-	if has_value(server_names, server.name) then
-		local additional_opts = require(namespace .. "settings." .. server.name)
-		opts = vim.tbl_deep_extend("force", additional_opts, opts)
+	if server == "sumneko_lua" then
+		local sumneko_opts = require("user.lsp.settings.sumneko_lua")
+		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
 	end
 
-	server:setup(opts)
-end)
+	if server == "pyright" then
+		local pyright_opts = require("user.lsp.settings.pyright")
+		opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+	end
 
+	lspconfig[server].setup(opts)
+end
