bc3b2aea93ade2c5404c2378db96ebff790105f5
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-03-02 16:00:25 +0300

    fix(nvim): add `gspbox` folder to telescope ignore
    Ignore `gspbox` directory in neovim-telescope.

--- .config/nvim/lua/user/telescope.lua
+++ .config/nvim/lua/user/telescope.lua
@@ -10,7 +10,7 @@ telescope.setup({
 		prompt_prefix = " ",
 		selection_caret = " ",
 		path_display = { "smart" },
-		file_ignore_patterns = { ".git/", "node_modules" },
+		file_ignore_patterns = { ".git", "node_modules", "gspbox" },
 
 		mappings = {
 			i = {
