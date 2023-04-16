0298c4fe85d77c307d3e4fbae8778b6422d4fe4f
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-04-16 17:01:00 +0300

    feat(nvim): export `LazyVim` plugins to own files
    Export the `LazyVim` plugins to own files while reversing the
    `example.lua` to original state for future reference.

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+	-- treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			-- add tsx and treesitter
+			vim.list_extend(opts.ensure_installed, {
+				"tsx",
+				"typescript",
+			})
+		end,
+	},
+}
