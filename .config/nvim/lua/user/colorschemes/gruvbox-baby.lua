6dc7f8984497e6676aa1a648cdfc3617b833e2ea
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-04-16 16:16:45 +0300

    feat: initialize conversion to `LazyVim`
    Initialize conversion to LazyVim by nuking the old `neovim`
    configuration and installing the `starter` of LazyVim.

--- .config/nvim/lua/user/colorschemes/gruvbox-baby.lua
+++
@@ -1,4 +0,0 @@
-vim.g.gruvbox_baby_background_color = "medium"
-vim.g.gruvbox_baby_function_style = "NONE"
-
-vim.cmd([[colorscheme gruvbox-baby]])
--- .config/nvim/lua/user/colorschemes/gruvbox-material.lua
+++
@@ -1,3 +0,0 @@
-vim.g.gruvbox_material_enable_italic_comment = 1
-
-vim.cmd([[colorscheme gruvbox-material]])
