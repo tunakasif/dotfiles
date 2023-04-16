99c9c8330350511fbd9e253ce1dcc301df02466d
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-04-17 02:52:38 +0300

    feat(mpv): add `video-latency-hacks` literally
    Add `--video-latency-hacks` option to `mpv` alias.

--- .scripts/alias.sh
+++ .scripts/alias.sh
@@ -34,5 +34,5 @@ alias icat='kitty +kitten icat'
 alias kittyconfig="$EDITOR $HOME/.config/kitty/kitty.conf"
 
 # mpv
-alias mpv='__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia mpv "$@"'
+alias mpv='__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia mpv --video-latency-hacks "$@"'
 
