774f3faf5c402b545c3013edc5bae583d9b309e0
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-04-19 20:46:23 +0300

    feat(mpv): move `mpv` wrapper to script
    The NVIDIA wrapper created for hybrid laptops is moved to a script under
    `~/.local/bin`.

--- .scripts/alias.sh
+++ .scripts/alias.sh
@@ -33,6 +33,3 @@ alias ohmyzsh="$EDITOR $HOME/.oh-my-zsh"
 alias icat='kitty +kitten icat'
 alias kittyconfig="$EDITOR $HOME/.config/kitty/kitty.conf"
 
-# mpv
-alias mpv='__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia mpv --video-latency-hacks "$@"'
-
