475dcb1a499deb88a70bd83e2a075e979c3a63c2
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-05-09 13:13:58 +0300

    refactor(nvim): move `vimtex` globals to `init`
    Move `vimtex` plugin global options to `init` function.

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -1,6 +1,7 @@
-vim.g.vimtex_view_method = "zathura"
-vim.g.vimtex_quickfix_open_on_warning = 0
-
 return {
 	"lervag/vimtex",
+	init = function()
+		vim.g.vimtex_view_method = "zathura"
+		vim.g.vimtex_quickfix_open_on_warning = 0
+	end,
 }
