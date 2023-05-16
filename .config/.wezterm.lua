677538996da3f1b8937ef6e797273db13a00f5dc
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-05-16 12:28:58 +0300

    feat(wezterm): add initial configuration
    Add initial configuration for Wezterm terminal emulator.

---
+++ .config/.wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Latte"
+	end
+end
+
+return {
+	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
+	font = wezterm.font("CaskaydiaCove Nerd Font"),
+	font_size = 18.0,
+}
