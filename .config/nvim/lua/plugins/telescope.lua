3b90b30ced004148acb125e3862507c1b39360c5
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-08-13 14:27:59 +0300

    fix(nvim): add `gspbox` to `telescope` ignore
    Ignore `gspbox` in telescope which is highly interferes with the search
    during MATLAB based GSP projects.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,17 +3,19 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
+			-- add a keymap to browse plugin files
+			{
+				"<leader>fp",
+				function()
+					require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+				end,
+				desc = "Find Plugin File",
+			},
 		},
 		config = function()
 			local actions = require("telescope.actions")
 			require("telescope").setup({
+				file_ignore_patterns = { ".git", "node_modules", "gspbox", "ltfat" },
 				defaults = {
 					mappings = {
 						i = {
