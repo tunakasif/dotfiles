630a4cdfa6ecb2b98d56bf104a375f34e1350e43
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-09-14 16:34:04 +0300

    feat(nvim): add `ltex-ls` and `ltex_extra`
    Add `ltex` language tool to Neovim setup with `ltex_extra` to
    fix code actions.

---
+++ .config/nvim/lua/plugins/ltex-extra.lua
@@ -0,0 +1,40 @@
+local function get_ignored_words(paths)
+	local words = {}
+	for _, path in ipairs(paths) do
+		local f = io.open(path)
+		if f then
+			for word in f:lines() do
+				table.insert(words, word)
+			end
+			f:close()
+		end
+	end
+	return words
+end
+
+local paths = {
+	vim.fn.stdpath("config") .. "/spell/ltex.dictionary.en-US.txt",
+	vim.fn.expand("%:p:h") .. "/spell/ltex.dictionary.en-US.txt",
+	vim.fn.expand("%:p:h") .. "/.vscode/ltex.dictionary.en-US.txt",
+	vim.fn.expand("%:p:h") .. "/ltex.dictionary.en-US.txt",
+}
+local words = get_ignored_words(paths)
+
+return {
+	"barreiroleo/ltex_extra.nvim",
+	ft = { "markdown", "tex", "gitcommit" },
+	dependencies = { "neovim/nvim-lspconfig" },
+	config = function()
+		require("ltex_extra").setup({
+			server_opts = {
+				settings = {
+					ltex = {
+						dictionary = {
+							["en-US"] = words,
+						},
+					},
+				},
+			},
+		})
+	end,
+}
