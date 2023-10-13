61270b8b2ef6028c200d400b1cebda525f7d43ea
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-10-13 21:35:40 +0300

    fix(nvim): adjust `LazyVim` extras
    Reorder LazyVim plugin extras and and none-ls
    extra for new formatting scheme.

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -12,14 +12,15 @@ require("lazy").setup({
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
 		-- import any extras modules here
+		{ import = "lazyvim.plugins.extras.coding.copilot" },
+		{ import = "lazyvim.plugins.extras.dap.core" },
 		{ import = "lazyvim.plugins.extras.lang.docker" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.python" },
 		{ import = "lazyvim.plugins.extras.lang.tex" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
-		{ import = "lazyvim.plugins.extras.coding.copilot" },
-		{ import = "lazyvim.plugins.extras.dap.core" },
+		{ import = "lazyvim.plugins.extras.lsp.none-ls" },
 
 		-- import/override with your plugins
 		{ import = "plugins" },
