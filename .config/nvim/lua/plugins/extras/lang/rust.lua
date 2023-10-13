cf30132f860dc1223d683de7fd79aaaa08d4715f
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-10-13 21:37:28 +0300

    fix(nvim): update lsp attach function
    With LazyVim 10.0.0, the `require("lazyvim.util").on_attach`
    is replaced with `require("lazyvim.util").lsp.on_attach`.

--- .config/nvim/lua/plugins/extras/lang/rust.lua
+++ .config/nvim/lua/plugins/extras/lang/rust.lua
@@ -42,7 +42,7 @@ return {
 			-- make sure mason installs the server
 			setup = {
 				rust_analyzer = function(_, opts)
-					require("lazyvim.util").on_attach(function(client, buffer)
+					require("lazyvim.util").lsp.on_attach(function(client, buffer)
             -- stylua: ignore
             if client.name == "rust_analyzer" then
               vim.keymap.set("n", "K", "<cmd>RustHoverActions<cr>", { buffer = buffer })
@@ -102,7 +102,7 @@ return {
 						end
 					end
 
-					require("lazyvim.util").on_attach(function(client, buffer)
+					require("lazyvim.util").lsp.on_attach(function(client, buffer)
             -- stylua: ignore
             if client.name == "taplo" then
               vim.keymap.set("n", "K", show_documentation, { buffer = buffer })
