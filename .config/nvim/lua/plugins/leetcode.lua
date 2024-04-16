a2effdac9eea429902f2cba3c0870db8b607bc51
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2024-04-16 14:17:21 +0300

    fix(nvim): remove leetcode `injector` option

--- .config/nvim/lua/plugins/leetcode.lua
+++ .config/nvim/lua/plugins/leetcode.lua
@@ -14,17 +14,5 @@ return {
 	},
 	opts = {
 		lang = "python3",
-		injector = { ---@type table<lc.lang, lc.inject>
-			["python3"] = {
-				before = "true",
-			},
-			["cpp"] = {
-				before = { "#include <bits/stdc++.h>", "using namespace std;" },
-				after = "int main() {}",
-			},
-			["java"] = {
-				before = "import java.util.*;",
-			},
-		},
 	},
 }
