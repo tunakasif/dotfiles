6b2e5c0d4f0e7b1b767de19eb8f272aeb1780497
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2024-05-01 14:36:00 +0300

    fix(nvim): update `telescope` file ignore patterns
    The ignore patterns included `.git`, which was resulting in
    ignorance of `.github/` also. Therefore, it is fixed to use
    `.git/`.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -14,7 +14,7 @@ return {
 		},
 		opts = {
 			defaults = {
-				file_ignore_patterns = { ".git", "node_modules", "gspbox/", "ltfat/" },
+				file_ignore_patterns = { ".git/", "node_modules", "gspbox/", "ltfat/" },
 				layout_strategy = "horizontal",
 				layout_config = { prompt_position = "top" },
 				sorting_strategy = "ascending",
