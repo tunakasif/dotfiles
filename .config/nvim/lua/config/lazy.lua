7b948f250d62dce317d9392eb674545d1fcd33a9
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2024-05-16 13:50:45 +0300

    feat(nvim): default to `LazyVim` `tex` config
    The previous configuration converge to the one provided directly
    by the LazyVim extra config. Therefore, default to the provided
    configuration with a few personal adjustments like setting the
    default PDF viewer zathura, setting `conceallevel` to 0, etc.

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.python" },
 		{ import = "lazyvim.plugins.extras.lang.rust" },
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
+		{ import = "lazyvim.plugins.extras.lang.tex" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
 		{ import = "lazyvim.plugins.extras.test.core" },
