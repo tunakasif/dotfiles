ce7e740b264e928c86079d593cc5d1c51efded87
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2024-05-30 00:22:56 +0300

    feat(nvim): add word-count for markdown files

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,6 +4,8 @@ return {
 		event = "VeryLazy",
 		opts = function(_, opts)
 			opts.options.section_separators = { left = "", right = "" }
+			local prose = require("nvim-prose")
+			table.insert(opts.sections.lualine_x, { prose.word_count, cond = prose.is_available })
 			return opts
 		end,
 	},
--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,4 +1,7 @@
 return {
 	-- disable headlines
 	{ "lukas-reineke/headlines.nvim", enabled = false },
+	{
+		"skwee357/nvim-prose",
+	},
 }
