c83ee6b44c76731ddcac42b500e2a8e37773186c
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2024-05-30 10:43:23 +0300

    feat(sh): update history options for `zsh`

--- .scripts/bindkey.sh
+++ .scripts/bindkey.sh
@@ -1,4 +1,6 @@
 bindkey -v
 bindkey -s '^o' 'lfcd^M'
-# bindkey '^j' history-substring-search-down
-# bindkey '^k' history-substring-search-up
+bindkey '^n' history-search-forward
+bindkey '^p' history-search-backward
+bindkey -M vicmd '^j' history-search-forward
+bindkey -M vicmd '^k' history-search-backward
