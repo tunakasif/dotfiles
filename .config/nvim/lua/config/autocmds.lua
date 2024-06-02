89c18a030a31d19d62170c55d4a5c2c54cb365ab
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2024-06-02 10:35:06 +0300

    feat(nvim): auto hard wrap at 80 characters for md

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Enable auto hard wrap at 80 characters for markdown files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "markdown" },
+	callback = function()
+		vim.opt_local.textwidth = 80
+		vim.opt_local.formatoptions:append("t")
+	end,
+})
