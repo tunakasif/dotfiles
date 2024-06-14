54872d3ed8f8c85bc3d06eb8950b4d8f1b35d11d
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2024-06-14 15:41:54 +0300

    feat(nvim): add `fzf` extra of `LazyVim`

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.coding.mini-surround" },
 		{ import = "lazyvim.plugins.extras.dap.core" },
 		{ import = "lazyvim.plugins.extras.dap.nlua" },
+		{ import = "lazyvim.plugins.extras.editor.fzf" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.lang.clangd" },
 		{ import = "lazyvim.plugins.extras.lang.docker" },
