011e092f52092788e90054cd913bbc0982858981
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2024-08-31 19:15:51 +0300

    fix(nvim): disable markdown auto line break

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -3,10 +3,10 @@
 -- Add any additional autocmds here
 
 -- Enable auto hard wrap at 80 characters for markdown files
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "markdown" },
-	callback = function()
-		vim.opt_local.textwidth = 80
-		vim.opt_local.formatoptions:append("t")
-	end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	pattern = { "markdown" },
+-- 	callback = function()
+-- 		vim.opt_local.textwidth = 80
+-- 		vim.opt_local.formatoptions:append("t")
+-- 	end,
+-- })
