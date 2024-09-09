fadc11acda2934cc3f0331f7fa3d641fdddb7055
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2024-09-09 11:04:37 +0300

    feat(nvim): add markdown render

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -4,4 +4,11 @@ return {
 	{
 		"skwee357/nvim-prose",
 	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		opts = {},
+		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+		-- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+		-- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+	},
 }
