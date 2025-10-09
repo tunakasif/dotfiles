1fe4aba6a1731b31213301cb8adadf2fff91c9f3
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2025-10-10 00:59:29 +0200

    feat(nix): add `zsh` configuration

--- .config/home-manager/modules/home-manager/default.nix
+++ .config/home-manager/modules/home-manager/default.nix
@@ -3,6 +3,34 @@ inputs: {
   programs.home-manager.enable = true;
   xdg.enable = true;
 
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;        # zsh-autosuggestions
+    enableSyntaxHighlighting = true;     # zsh-syntax-highlighting
+
+    history.size = 10000;
+
+    # Oh My Zsh
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "docker"
+        "fzf"
+        "git"
+      ];
+      # You can also add your own plugins by dropping files under ~/.config/zsh or with home.file (see below)
+    };
+
+    # Extra zshrc snippets
+    initExtra = ''
+      export EDITOR=nvim
+
+      alias ll="ls -lah"
+      alias gs="git status"
+    '';
+  };
+
   # Individual imports
   imports = [
     ./new.nix
