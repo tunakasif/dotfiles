1b0dc0f7a63a3e6d234ecc84ae05f20b7852ecff
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2026-01-05 01:38:03 +0300

    fix(nix): update git configuration

--- nix-config/modules/home-manager/git.nix
+++ nix-config/modules/home-manager/git.nix
@@ -6,59 +6,48 @@
 }:
 {
   programs = {
-    # Optional but nice: delta integration via HM module
-    delta = {
-      enable = true;
-      enableGitIntegration = true;
-    };
-
     git = {
       enable = true;
-      userName = user.name;
-      userEmail = user.email;
-
-      # Global ignores (like ~/.config/git/ignore)
+      settings = {
+        gpg.format = "ssh";
+        tag.gpgsign = true;
+        commit.gpgsign = true;
+        user = {
+          inherit (user) name email;
+          signingkey = "${config.home.homeDirectory}/.ssh/id_rsa.pub";
+        };
+        init.defaultBranch = "main";
+        push = {
+          followTags = true;
+          autoSetupRemote = true;
+        };
+        pull = {
+          rebase = true;
+        };
+        fetch = {
+          prune = true;
+          pruneTags = true;
+        };
+      };
       ignores = [
         ".direnv/"
         ".DS_Store"
         "*.swp"
       ];
-
-      # Main gitconfig (writes to ~/.config/git/config)
-      settings = {
-        init.defaultBranch = "main";
-
-        pull.rebase = true;
-        fetch.prune = true;
-        push.autoSetupRemote = true;
-
-        core.editor = "nvim";
-
-        # Commit signing example: SSH signing (popular alternative to GPG)
-        gpg.format = "ssh";
-        user.signingkey = "${config.home.homeDirectory}/.ssh/id_rsa.pub";
-        commit.gpgsign = true;
-        tag.gpgsign = true;
-      };
     };
 
     gh = {
       enable = true;
-      settings = {
-        git_protocol = "https";
-        prompt = "enabled";
-      };
-      gitCredentialHelper = {
-        enable = true;
-        hosts = [
-          "https://github.com"
-          "https://gist.github.com"
-        ];
-      };
+      gitCredentialHelper.enable = true;
       extensions = with pkgs; [
         gh-dash
         gh-f
       ];
+    };
+
+    delta = {
+      enable = true;
+      enableGitIntegration = true;
     };
   };
 }
