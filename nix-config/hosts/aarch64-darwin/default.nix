ba49c531cd32818e6829e27b67fb2a1460c0798b
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2026-01-04 22:14:35 +0300

    fix(nix): update darwin host to use common module

--- nix-config/hosts/aarch64-darwin/default.nix
+++ nix-config/hosts/aarch64-darwin/default.nix
@@ -1,13 +1,13 @@
-{
-  pkgs,
-  user,
-  inputs,
-  ...
-}:
+{ user, ... }:
 let
   uid = 501; # User ID created by MacOS for the user use `id -u` to get it.
 in
 {
+  imports = [
+    ../../modules/darwin
+  ];
+
+  # Host-specific user configuration
   users = {
     knownUsers = [ user.username ];
     users.${user.username} = {
