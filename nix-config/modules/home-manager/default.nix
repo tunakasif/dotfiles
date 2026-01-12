50dc18fc23434bccdcef4c268848776743004cf2
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2026-01-13 00:10:25 +0100

    feat(nix): update `opencode` configuration

--- nix-config/modules/home-manager/default.nix
+++ nix-config/modules/home-manager/default.nix
@@ -11,6 +11,52 @@
       enable = true;
       settings = {
         theme = "catppuccin";
+        mcp = {
+          filesystem = {
+            enabled = true;
+            type = "local";
+            command = [
+              "npx"
+              "-y"
+              "@modelcontextprotocol/server-filesystem"
+              "."
+            ];
+          };
+          fetch = {
+            enabled = true;
+            type = "local";
+            command = [
+              "uvx"
+              "mcp-server-fetch"
+            ];
+          };
+          git = {
+            enabled = true;
+            type = "local";
+            command = [
+              "uvx"
+              "mcp-server-git"
+            ];
+          };
+          rg = {
+            enabled = true;
+            type = "local";
+            command = [
+              "npx"
+              "-y"
+              "mcp-ripgrep"
+            ];
+          };
+          pdf-reader = {
+            enabled = false;
+            type = "local";
+            command = [
+              "npx"
+              "-y"
+              "@sylphx/pdf-reader-mcp"
+            ];
+          };
+        };
       };
     };
     zathura.enable = true;
