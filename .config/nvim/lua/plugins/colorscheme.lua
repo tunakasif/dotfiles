8fdf767296cf53d8b8117a083dd888c712ff8ce2
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2026-05-14 10:29:00 +0200

    fix(nvim): update latest catppuccin theme settings

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -87,35 +87,28 @@ return {
 		"catppuccin/nvim",
 		lazy = false,
 		name = "catppuccin",
+		priority = 1000,
 		opts = {
-			transparent_background = true,
+			flavour = "auto", -- latte, frappe, macchiato, mocha
+			background = { -- :h background
+				light = "latte",
+				dark = "mocha",
+			},
+			term_colors = true,
+			transparent_background = true, -- disables setting the background color.
+			float = {
+				transparent = false, -- enable transparent floating windows
+				solid = false, -- use solid styling for floating windows, see |winborder|
+			},
 			integrations = {
-				alpha = true,
-				cmp = true,
 				flash = true,
-				gitsigns = true,
-				illuminate = true,
-				indent_blankline = { enabled = true },
 				lsp_trouble = true,
 				mason = true,
-				mini = true,
-				native_lsp = {
-					enabled = true,
-					underlines = {
-						errors = { "undercurl" },
-						hints = { "undercurl" },
-						warnings = { "undercurl" },
-						information = { "undercurl" },
-					},
-				},
 				navic = { enabled = true, custom_bg = "lualine" },
 				neotest = true,
+				neotree = true,
 				noice = true,
 				notify = true,
-				neotree = true,
-				semantic_tokens = true,
-				telescope = true,
-				treesitter = true,
 				which_key = true,
 			},
 		},
