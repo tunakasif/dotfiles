e197d5800dd7df43f083349e15b90213129502bb
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2026-06-06 15:19:39 +0200

    refactor: apply `shellcheck` and `shfmt`

--- .local/share/pandoc/templates/download_latest_eisvogel.sh
+++ .local/share/pandoc/templates/download_latest_eisvogel.sh
@@ -5,9 +5,9 @@ gh release download -R Wandmalfarbe/pandoc-latex-template -p 'Eisvogel-*.zip'
 
 # find first zip file containing pandoc in its name and unzip it
 zipname=$(find . -name "Eisvogel-*.zip" | head -n 1)
-basename=$(basename $zipname .zip)
+basename=$(basename "$zipname" .zip)
 
 # extract the latest `eisvogel.tex` from zip file
 # and copy it to the current directory
 unzip -qq -j -o "$zipname" "$basename/eisvogel.latex" "$basename/eisvogel.beamer"
-rm $zipname
+rm "$zipname"
