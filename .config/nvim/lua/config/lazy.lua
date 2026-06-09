tunakasif/dotfiles

commit 584cc35f6dab5c5d138008fee82a8abbcd3db1ac

fix(nvim): disable `refactoring` extra for now
With recent updates, LazyVim-refactoring.nvim extra is broken. Disable
it until resolved.

.config/nvim/lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.coding.mini-surround" },
 		{ import = "lazyvim.plugins.extras.dap.core" },
 		{ import = "lazyvim.plugins.extras.dap.nlua" },
-		{ import = "lazyvim.plugins.extras.editor.refactoring" },
+		-- { import = "lazyvim.plugins.extras.editor.refactoring" },
 		{ import = "lazyvim.plugins.extras.editor.telescope" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.lang.clangd" },

commit 23cd7c083fcd3caaa443281d2cd6eb2d7e12f626

fix(nvim): re-enable `refactoring.nvim`
The broken plugin was disabled in 584cc35, but it is fixed with LazyVim
16, so re-enabling it now that the fix is available.

Refs: 584cc35

.config/nvim/lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.coding.mini-surround" },
 		{ import = "lazyvim.plugins.extras.dap.core" },
 		{ import = "lazyvim.plugins.extras.dap.nlua" },
-		-- { import = "lazyvim.plugins.extras.editor.refactoring" },
+		{ import = "lazyvim.plugins.extras.editor.refactoring" },
 		{ import = "lazyvim.plugins.extras.editor.telescope" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.lang.clangd" },