65f8627b11e042c1ee563f4738920b6f96bc1c78
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2026-07-08 17:17:29 +0200

    feat(nvim): add `LintInfo` user command
    When `nvim-lint` attaches, add LintInfo command to see configured and
    running linters for that buffer/filetype.

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -1,6 +1,17 @@
 return {
 	"mfussenegger/nvim-lint",
 	opts = function(_, opts)
+		vim.api.nvim_create_user_command("LintInfo", function()
+			local lint = require("lint")
+			local ft = vim.bo.filetype
+			local configured = lint.linters_by_ft[ft] or {}
+			local running = lint.get_running(0)
+
+			print("filetype: " .. ft)
+			print("configured linters: " .. vim.inspect(configured))
+			print("running linters: " .. vim.inspect(running))
+		end, {})
+
 		opts.linters_by_ft = vim.tbl_extend("force", opts.linters_by_ft or {}, {
 			markdown = { "markdownlint-cli2" },
 			nix = { "statix" },
