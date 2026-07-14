20c2d352bf98a22ef0ff98ec31f79184cbfaf5d2
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2026-07-15 00:59:01 +0200

    fix(nix): set `lts4mac54` login shell to zsh

--- nix-config/hosts/lts4mac54/default.nix
+++ nix-config/hosts/lts4mac54/default.nix
@@ -1,9 +1,16 @@
-{user, ...}: let
+{
+  pkgs,
+  user,
+  ...
+}: let
   uid = 501; # User ID created by MacOS for the user use `id -u` to get it.
 in {
   imports = [
     ../../modules/darwin
   ];
+
+  # Register the nix-maintained zsh as a valid login shell.
+  environment.shells = [pkgs.zsh];
 
   # Host-specific user configuration
   users = {
@@ -12,6 +19,7 @@ in {
       name = user.username;
       description = user.name;
       home = "/Users/${user.username}";
+      shell = pkgs.zsh;
       inherit uid;
     };
   };
