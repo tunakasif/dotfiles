e4f85ba331b049e8c392f519ea1df16886a5889d
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2026-09-16 19:34:14 +0200

    fix(nvim): update `render-markdown` config

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -6,7 +6,20 @@ return {
 	},
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
-		opts = {},
+		opts = {
+			code = {
+				sign = false,
+				width = "block",
+				right_pad = 1,
+			},
+			heading = {
+				sign = false,
+				icons = {},
+			},
+			checkbox = {
+				enabled = true,
+			},
+		},
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
 		-- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
 		-- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
